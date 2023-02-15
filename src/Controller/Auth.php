8cf2f5d3a765a6db793a618d04b4f291f8652b09
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-15 17:06:12 +0600

    Добавлена авторизация

--- src/Controller/Auth.php
+++ src/Controller/Auth.php
@@ -9,17 +9,39 @@ class Auth  extends Controller
 
 	public function action_login()
 	{
-
+		return $this->render('tmpl/auth.html.php', [
+			'title' => 'Авторизуйтесь для продолжения'
+		]);
 	}
 
 	public function action_authenticate()
 	{
+		$dataAuth = $this->request->getBody();
+		if ($dataAuth['login'] == 'admin' && $dataAuth['password'] == 'admin')
+		{
+			setcookie(
+				'auth',
+				md5(time()),
+				time() + 60 * 60
+				//,'/', $_SERVER['HTTP_HOST'], false, true
+			);
+			return $this->renderJson([], 204);
+		}
 
+		return $this->renderJson([
+			'message' => 'Неверный логин или пароль'
+		], 401);
 	}
 
 
 	public function action_logout()
 	{
-
+		setcookie(
+			'auth',
+			0,
+			time() - 60 * 60
+		//,'/', $_SERVER['HTTP_HOST'], false, true
+		);
+		return $this->redirect('/');
 	}
 }
