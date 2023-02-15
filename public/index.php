e275678d3cbf0ec1a3222e69bd8b4723e37eefb7
artbeard <frolov.web@gmail.com>
2023-02-15 10:01:50 +0600

    init

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+define('PUBLIC_DIR', __DIR__ . DIRECTORY_SEPARATOR);
+define('APP_DIR', realpath(__DIR__ . '/../src/') . DIRECTORY_SEPARATOR);
+
+require_once APP_DIR.'../vendor/autoload.php';
