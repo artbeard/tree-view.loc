57ad191b2e51dba3adb83e560e31706260b7c1ec
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-15 17:05:15 +0600

    Добавлена базовая разметка и стилистика

--- src/tmpl/auth.html.php
+++ src/tmpl/auth.html.php
@@ -3,13 +3,82 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+			content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="/assets/css/style.css">
 	<title><?= isset($title) ? $title : '' ?></title>
 </head>
 <body>
+<div class="container">
+	<nav>
+		<ul class="menu">
+			<li class="menuItem">
+				<a href="/">Главная</a>
+			</li>
+			<li class="menuItem">
+				<a href="/admin">Админка</a>
+			</li>
+			<li class="menuItem">
+				<a href="/logout">Выход</a>
+			</li>
+		</ul>
+	</nav>
+	<main>
+		<h1><?= isset($title) ? $title : '' ?></h1>
+		<div class="authform">
+			<form id="authForm">
+				<div class="form-row hidden">
+					<span class="errorInfo"></span>
+				</div>
+				<div class="form-row">
+					<input type="text" name="login" required placeholder="login: admin">
+				</div>
+				<div class="form-row">
+					<input type="password" name="password" required placeholder="password: admin">
+				</div>
+				<div class="form-row">
+					<button>Вход</button>
+				</div>
+			</form>
+		</div>
+	</main>
+</div>
+<script>
+	document.addEventListener('DOMContentLoaded', function (){
+		document.getElementById('authForm').addEventListener('submit', function (event){
+			event.preventDefault()
+			const login = this.querySelector('input[name="login"]');
+			const password = this.querySelector('input[name="password"]');
+			const errorInfo = this.querySelector('span.errorInfo');
+			errorInfo.parentNode.classList.add('hidden');
 
-<?= isset($body) ? $body : '' ?>
+			fetch('/login', {
+				method: 'POST',
+				body: JSON.stringify({login: login.value, password: password.value})
+			})
+			.then(response => {
+				if (response.status >= 200 && response.status < 300)
+				{
+					//Авторизация прошла успешно
+					document.location.replace('/admin');
+				}
+				return response.json();
+			})
+			.then(data => {
+				if (data.message)
+				{
+					errorInfo.innerText = data.message;
+					errorInfo.parentNode.classList.remove('hidden');
+				}
+			})
+			.catch(err => {
+				console.log(err);
+				errorInfo.innerText = 'Ошибка запроса';
+				errorInfo.parentNode.classList.remove('hidden');
+			})
 
+		})
+	})
+</script>
 </body>
 </html>
--- src/tmpl/home.html.php
+++ src/tmpl/home.html.php
@@ -3,13 +3,62 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+			content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="/assets/css/style.css">
 	<title><?= isset($title) ? $title : '' ?></title>
 </head>
 <body>
+<div class="container">
+	<nav>
+		<ul class="menu">
+			<li class="menuItem">
+				<a href="/">Главная</a>
+			</li>
+			<li class="menuItem">
+				<a href="/admin">Админка</a>
+			</li>
+			<li class="menuItem">
+				<a href="/logout">Выход</a>
+			</li>
+		</ul>
+	</nav>
+	<main>
+		<h1><?= isset($title) ? $title : '' ?></h1>
 
-<?= isset($body) ? $body : '' ?>
+		<pre>
+		<?php
+		function print_tree($data)
+		{
+			echo '<ul>';
+			foreach ($data as $node)
+			{
+				echo '<li>';
+				echo $node['title'];
+				if (!empty($node['child']))
+				{
+					print_tree($node['child']);
+				}
+				echo '</li>';
+			}
+			echo '</ul>';
+		}
+		//$list
+		if (isset($list))
+		{
+			//echo print_r($list, true);
+			//print_r('133');
+			print_tree($list);
+		}
 
+		?>
+		</pre>
+
+	</main>
+</div>
+<script>
+	document.addEventListener('DOMContentLoaded', function (){
+	})
+</script>
 </body>
 </html>
